0b69e40ca1de7416265a16847cfe966149a9621a
Alpha Keita <78216570+alphakta@users.noreply.github.com>
2022-09-20 19:43:15 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ITodo } from './Todo'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-list';
+  todos: ITodo[] = []
+  newTodo: string;
+
+  addTodo() {
+    if (this.newTodo) {
+      this.todos.push({ content: this.newTodo, isCompleted: false })
+      this.initNewTodo()
+    }
+    else alert('Entrez une tâche')
+  }
+
+  initNewTodo() { this.newTodo = '' }
+  deleteTodo(i: number) { this.todos.splice(i, 1) }
+  done(i: number) { this.todos[i].isCompleted = !this.todos[i].isCompleted }
 }
